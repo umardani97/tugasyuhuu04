3bb458c3788770afb527e878a8834bf88270931a
umardani97 <32447378+dedidwisetiawan@users.noreply.github.com>
2018-01-01 21:51:18 +0800

    matrik nilai rR * W

--- tugasyuhuu04.cpp
+++ tugasyuhuu04.cpp
@@ -36,4 +36,13 @@ int main(){
 	for(int i=0;i<5;i++){
 		cout << w[i]<<" | ";
 	}cout <<endl;//Akhir dari Bobot W
+	
+	// 4# Matrix R * W
+	cout <<endl<< "MATRIKS R * W : "<<endl; 
+	double RWac[3][5];
+	for(int i=0;i<3;i++){
+		for(int j=0;j<5;j++){
+			RWac[i][j]=Rac[i][j]*w[j]; cout <<RWac[i][j]<<" | ";
+		}cout <<endl;
+	}//4# Akhir Matrix R * W
 }
