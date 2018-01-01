b7486dcc62f8371da28b97bee0f076ef10742686
umardani97 <32365705+umardani97@users.noreply.github.com>
2018-01-01 22:07:51 +0800

    Matrix X isi dan tampilaan data

--- tugasyuhuu0401.cpp
+++ tugasyuhuu0401.cpp
@@ -2,3 +2,14 @@
 #include <cmath>
 using namespace std;
 
+int main(){
+	cout.setf(ios::fixed); cout.setf(ios::showpoint); cout.precision(4);
+	// 1# Matrix X - Isi dan Tampilan Data
+	cout << "MATRIKS X : "<<endl;
+	double ac[3][5]={ {4,4,5,3,3},{3,3,4,2,3},{5,4,2,2,2} };
+	for(int i=0;i<3;i++){
+		for(int j=0;j<5;j++){
+			cout << ac[i][j] << " | ";
+		}cout<<endl;
+	}// Akhir dari Matrix X
+}
